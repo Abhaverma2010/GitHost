7277814470bece088d9aa442c0e339b6fa604134
Abha Verma <abhaverma1205@gmail.com>
2026-09-14 00:09:24 +0530

    Remove AWS access keys from aws-config.js
    Removed sensitive AWS access keys from configuration file.

--- backend/config/aws-config.js
+++ backend/config/aws-config.js
@@ -6,5 +6,3 @@ const s3 = new AWS.S3();
 const S3_BUCKET = "abhasbucckket";
 
 module.exports = { s3, S3_BUCKET };
-//1dN/7NqkCYqtzEW3VuXzWLxuhyg00nWSRlD82luw - secret access key
-//AKIATLT3C5BZVTBBHUDR - access key
